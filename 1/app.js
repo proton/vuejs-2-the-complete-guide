82d9f99586610d2aaef47f1858026005cc1732c1
Petr Savichev (proton) <psavichev@gmail.com>
2021-10-04 01:02:41 +0300

    assignment 1

---
+++ 1/app.js
@@ -0,0 +1,11 @@
+const App = {
+  data() {
+    return {
+      name: 'Petr Savichev (proton)',
+      age: 32,
+      imageUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+    }
+  }
+}
+
+Vue.createApp(App).mount('#assignment')
