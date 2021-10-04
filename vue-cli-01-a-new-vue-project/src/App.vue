559bba335d87380371f555fe28446ddd356214b6
Petr Savichev (proton) <psavichev@gmail.com>
2021-10-05 01:02:34 +0300

    83. Creating a Basic Vue App

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li v-for="friend in friends" :key="friend.id">
+        {{ friend.name }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: 'Petr Savichev',
+          phone: '123456789',
+          email: 'psavichev@gmail.com'
+        },
+        {
+          id: 1,
+          name: 'Mr President',
+          phone: '123456789',
+          email: 'some@example.com'
+        },
+      ]
+    }
+  },
+}
+</script>
