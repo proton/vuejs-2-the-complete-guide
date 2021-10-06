2fe6b074b1cfe6f08de74ca0d578a358223df2a8
Petr Savichev (proton) <psavichev@gmail.com>
2021-10-06 17:38:55 +0300

    lesson 125: First Components & Props

---
+++ prj-cmp-01/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <learning-resource v-for="res in storedResources" :key="res.id" :resource="res"></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        { id: 1, title: 'proton.name', description: "Peter Savichev's website", link: 'https://proton.name' },
+        { id: 2, title: 'Google', description: "World's most popular search engine", link: 'https://google.com' },
+      ],
+    }
+  },
+}
+</script>
---
+++ prj-cmp-01/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ resource.title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ resource.description }}</p>
+    <nav>
+      <a href>ViewResource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['resource'],
+}
+</script>
