2f9a4ca6610c386960bbd79cae4a374d0547a1bf
Petr Savichev (proton) <psavichev@gmail.com>
2021-10-09 22:28:12 +0300

    add vuex

--- vuex-01/src/main.js
+++ vuex-01/src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
 
-import App from './App.vue';
+import App from './App.vue'
 
-const app = createApp(App);
+const store = createStore({
+  state() {
+    return {
+      counter: 0
+    }
+  }
+})
 
-app.mount('#app');
+const app = createApp(App)
+app.use(store)
+
+app.mount('#app')
